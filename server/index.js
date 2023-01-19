7f5579b1502c24aff1045041515540db809db891
Girish Verma <78799435+GirishVerm@users.noreply.github.com>
2023-01-19 01:34:17 -0330

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
-const express = require("express")
-const app = express()
-const mongoose = require('mongoose')
-const UserModel = require('./models/users')
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const UserModel = require('./models/users');
 const cors = require("cors");
+const cheerio = require("cheerio");
+const axios = require('axios');
+
+const { JSDOM } = require('jsdom');
+
 
 app.use(express.json());
 app.use(cors());
@@ -10,6 +15,26 @@ app.use(cors());
 mongoose.connect(
     "mongodb+srv://Ayazzaman:AyAz1248@cluster0.bcfwkpv.mongodb.net/dhall-mern?retryWrites=true&w=majority"
 );
+
+app.get("/initData", (req, res) => {
+    axios
+    .get("https://mun.campusdish.com/en/locationsandmenus/gushuedininghall/")
+    .then((response) => {
+        ///console.log(response.data);
+        const $ = cheerio.load(response.data);
+        ///const htmlElement = $(".text/javascript");
+
+        const { document } = new JSDOM(response.data).window;
+
+        const scripts = document.getElementsByTagName("script");
+        console.log(scripts[16].outerHTML);
+        ///console.log(response.data);        
+       
+    })
+    .catch((error) => {
+        console.error(error);
+    })
+});
 
 app.get("/getUsers", (req, res) => {
     UserModel.find({}, (err, result) => {
